faf01870178a14340ca887ba0a35d024d34f626e
27-TRM-02 <trms2702@gmail.com>
2021-10-28 19:25:09 +0200

    Funciones implementadas para DOM

---
+++ src/MainWindow.java
@@ -0,0 +1,282 @@
+
+import java.io.File;
+import javax.swing.JFileChooser;
+// import javax.xml.parsers.*;
+// import org.w3c.dom.*;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author trm
+ */
+public class MainWindow extends javax.swing.JFrame {
+
+    static MetodosDOM metodosDOM;
+    static int res;
+//    DOM ;
+//    SAX ;
+//    JAXB ;
+
+    public MainWindow() {
+        initComponents();
+    }
+
+    private File dialogoSeleccionFichero() {
+        File f = null;
+        JFileChooser jfc = new JFileChooser();
+        jfc.setMultiSelectionEnabled(false);
+        jfc.setDialogType(JFileChooser.OPEN_DIALOG);
+        // guarda con que se ha cerrado el file chooser
+        int res = jfc.showOpenDialog(this);
+
+        if (res == JFileChooser.APPROVE_OPTION) {
+            f = jfc.getSelectedFile();
+        }
+        return f;
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        estadoLabel = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTextArea1 = new javax.swing.JTextArea();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        tituloText = new javax.swing.JTextPane();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        annoText = new javax.swing.JTextPane();
+        jScrollPane4 = new javax.swing.JScrollPane();
+        autorText = new javax.swing.JTextPane();
+        addLibroBoton = new javax.swing.JButton();
+        mostrarDOMboton = new javax.swing.JButton();
+        guardarDOM = new javax.swing.JButton();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu1 = new javax.swing.JMenu();
+        jMenuItem1 = new javax.swing.JMenuItem();
+        jMenuItem2 = new javax.swing.JMenuItem();
+        jMenuItem3 = new javax.swing.JMenuItem();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jTextArea1.setColumns(20);
+        jTextArea1.setRows(5);
+        jScrollPane1.setViewportView(jTextArea1);
+
+        jLabel1.setText("Título:");
+
+        jLabel2.setText("Autor:");
+
+        jLabel3.setText("Publicado en:");
+
+        jScrollPane3.setViewportView(tituloText);
+
+        jScrollPane2.setViewportView(annoText);
+
+        jScrollPane4.setViewportView(autorText);
+
+        addLibroBoton.setText("Añadir Libro");
+        addLibroBoton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                addLibroBotonActionPerformed(evt);
+            }
+        });
+
+        mostrarDOMboton.setText("Mostrar DOM ");
+        mostrarDOMboton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                mostrarDOMbotonActionPerformed(evt);
+            }
+        });
+
+        guardarDOM.setText("Guardar DOM a File");
+        guardarDOM.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                guardarDOMActionPerformed(evt);
+            }
+        });
+
+        jMenu1.setText("Ficheros XML");
+
+        jMenuItem1.setText("Abrir DOM");
+        jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem1ActionPerformed(evt);
+            }
+        });
+        jMenu1.add(jMenuItem1);
+
+        jMenuItem2.setText("jMenuItem2");
+        jMenu1.add(jMenuItem2);
+
+        jMenuItem3.setText("jMenuItem3");
+        jMenu1.add(jMenuItem3);
+
+        jMenuBar1.add(jMenu1);
+
+        setJMenuBar(jMenuBar1);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                    .addComponent(mostrarDOMboton, javax.swing.GroupLayout.DEFAULT_SIZE, 282, Short.MAX_VALUE)
+                    .addComponent(estadoLabel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 282, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jScrollPane3, javax.swing.GroupLayout.DEFAULT_SIZE, 343, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel2)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jScrollPane4))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel3)
+                        .addGap(18, 18, 18)
+                        .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 137, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(addLibroBoton, javax.swing.GroupLayout.PREFERRED_SIZE, 115, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(guardarDOM, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(24, 24, 24))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(estadoLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jScrollPane3, javax.swing.GroupLayout.DEFAULT_SIZE, 32, Short.MAX_VALUE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, 32, Short.MAX_VALUE)
+                            .addComponent(jScrollPane4))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, 32, Short.MAX_VALUE)
+                                .addComponent(jScrollPane2))
+                            .addComponent(addLibroBoton))
+                        .addGap(18, 18, 18)
+                        .addComponent(guardarDOM))
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 292, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addComponent(mostrarDOMboton, javax.swing.GroupLayout.PREFERRED_SIZE, 46, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
+        File fichero = dialogoSeleccionFichero();
+        res = metodosDOM.abrir_XML_DOM(fichero);
+        if (res == 0) {
+            estadoLabel.setText("Se ha creado el arbol correctamente");
+        } else {
+            estadoLabel.setText("Fallo en leer el xml. Seleccione otro archivo");
+        }
+    }//GEN-LAST:event_jMenuItem1ActionPerformed
+
+    private void addLibroBotonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_addLibroBotonActionPerformed
+        // TODO add your handling code here:
+        res = metodosDOM.annadirLibroDOM(tituloText.getText(), autorText.getText(), annoText.getText());
+        if (res == 0) {
+            estadoLabel.setText("Se ha añadido el libro correctamente");
+        } else if (res == -1) {
+            estadoLabel.setText("No se ha podido añadir el libro");
+        }
+    }//GEN-LAST:event_addLibroBotonActionPerformed
+
+    private void mostrarDOMbotonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mostrarDOMbotonActionPerformed
+        // TODO add your handling code here:
+        jTextArea1.setText(metodosDOM.recorrerDOMyMostrar());
+    }//GEN-LAST:event_mostrarDOMbotonActionPerformed
+
+    private void guardarDOMActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_guardarDOMActionPerformed
+        // TODO add your handling code here:
+        JFileChooser saveDOM = new JFileChooser();
+        saveDOM.showSaveDialog(this);
+        res = metodosDOM.guardarDOMcomoFile(saveDOM.getSelectedFile());
+        if (res == 0) {
+            estadoLabel.setText("Se ha guardado correctamente");
+        } else if (res == -1) {
+            estadoLabel.setText("No se ha podido guardar");
+        }
+    }//GEN-LAST:event_guardarDOMActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        metodosDOM = new MetodosDOM();
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(MainWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(MainWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(MainWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(MainWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new MainWindow().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton addLibroBoton;
+    private javax.swing.JTextPane annoText;
+    private javax.swing.JTextPane autorText;
+    private javax.swing.JLabel estadoLabel;
+    private javax.swing.JButton guardarDOM;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JMenuItem jMenuItem2;
+    private javax.swing.JMenuItem jMenuItem3;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JScrollPane jScrollPane4;
+    private javax.swing.JTextArea jTextArea1;
+    private javax.swing.JButton mostrarDOMboton;
+    private javax.swing.JTextPane tituloText;
+    // End of variables declaration//GEN-END:variables
+}
---
+++ src/MetodosDOM.java
@@ -0,0 +1,169 @@
+
+import com.sun.org.apache.xml.internal.serialize.OutputFormat;
+import com.sun.org.apache.xml.internal.serialize.XMLSerializer;
+import java.io.File;
+import java.io.FileOutputStream;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author trm
+ */
+public class MetodosDOM {
+    Document doc;
+    /**
+     * 
+     * @param fichero
+     * @return 
+     */
+    public int abrir_XML_DOM(File fichero) {
+        // doc representará el árbol DOM
+        doc = null;
+        try {
+            // Se crea un objeto DocumentBuilderFactory
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            // Indica que el modelo DOM no debe contemplar los comentarios 
+            // que tenga el XML.
+            factory.setIgnoringComments(true);
+            // Ignora los espacios en blanco que tenga el documento
+            factory.setIgnoringElementContentWhitespace(true);
+            // Creamos objeto para cargar la estructura del arbol del DOM
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            // Parsea el documento XML y genera el DOM equivalente
+            doc = builder.parse(fichero);
+            // Nos apunta al arbol DOM listo para ser recorrido 
+            return 0;
+        } catch (Exception e) {
+            return -1;
+        }
+    }
+    
+    /**
+     * 
+     * @param titulo
+     * @param autor
+     * @param anno
+     * @return 
+     */
+    public int annadirLibroDOM(String titulo, String autor, String anno){
+        try {
+            // Se crea un nodo tipo Element con nombre Titulo
+            Node ntitulo = doc.createElement("Titulo");
+            // Nodo texto con el titulo del libro
+            Node ntitulo_text = doc.createTextNode(titulo); 
+            // Nodo texto como hijo de Titulo
+            ntitulo.appendChild(ntitulo_text);
+            // Hacemos lo mismo con el Element Autor
+            Node nautor = doc.createElement("Autor");
+            Node nautor_text = doc.createTextNode(autor); 
+            nautor.appendChild(nautor_text);
+            // Creamos Nodo Element de Libro
+            Node nlibro = doc.createElement("Libro");
+            // Al nodo Libro se le añade atributo "publicado_en"
+            ((Element)nlibro).setAttribute("publicado_en", anno);
+            // Añadimos a libro los nodos Titulo y Autor
+            nlibro.appendChild(ntitulo);
+            nlibro.appendChild(nautor);
+            // Obtenemos primer nodo del doc --> (Libros)
+            Node raiz = doc.getFirstChild();
+            // añadimos como hijo Libro creado arriba
+            raiz.appendChild(nlibro);
+            
+            return 0;
+        } catch (Exception e) {
+            System.out.println(e.toString());
+            return -1;
+        }
+    }
+    
+    /**
+     * 
+     * @return 
+     */
+    public String recorrerDOMyMostrar(){
+        String salida = "";
+        Node node;
+        String datos_nodo[]=null;
+        // Obtiene el primer nodo del DOM (primer hijo) = LIBROS
+        Node raiz = doc.getFirstChild();
+        // Obtiene lista de nodos con todos los nodos hijo del raíz = LIBRO * 3
+        NodeList nodelist = raiz.getChildNodes();
+        // Procesa los nodos hijos
+        for (int i=0; i<nodelist.getLength(); i++) {
+            node = nodelist.item(i);
+            
+            if (node.getNodeType()==Node.ELEMENT_NODE){
+                // Es un nodo elemento = libro
+                datos_nodo = procesarLibro(node);
+                salida+= "\n " + "Publicado en: " + datos_nodo[0];
+                salida+= "\n " + "El Título es: " + datos_nodo[1];
+                salida+= "\n " + "El Autor es: " + datos_nodo[2];
+                salida+= "\n ----------------------------------";
+            }
+        }       
+        return salida;
+    }
+    
+    /**
+     * 
+     * @param n
+     * @return 
+     */
+    private String[] procesarLibro(Node n){ // n= nodo libro
+        String datos[] = new String[3];
+        Node ntemp = null;
+        int contador = 1;
+        // Obtiene el valor del primer atributo del nodo.
+        // En libros solo hay 1 elem. por eso no hace falta bucle
+        datos[0] = n.getAttributes().item(0).getNodeValue(); // = valor del atributo
+        // Obtiene los hijos del Libro (titulo y autor)
+        NodeList nodos = n.getChildNodes();
+        // Recorre todos los nodos de la lista 'nodos'
+        for (int i = 0; i < nodos.getLength(); i++) {
+            ntemp = nodos.item(i);
+            // Comprobamos que es nodo Elemento (titulo y autor)
+            if(ntemp.getNodeType() == Node.ELEMENT_NODE){
+                datos[contador] = ntemp.getFirstChild().getNodeValue();
+                contador++;
+            }
+        }
+        return datos;
+    }
+    
+    /**
+     * 
+     * @return 
+     */
+    public int guardarDOMcomoFile(File archivo){
+        try {
+            // Crea un fichero llamado salida.xml
+            // File archivo_xml = new File("salida.xml");
+            // Especifica el formato de salida
+            OutputFormat format = new OutputFormat(doc);
+            format.setIndenting(true);
+            // salida indentada
+            format.setIndenting(true);
+            // Escribo contenido en el File
+            XMLSerializer serializer = new XMLSerializer(new FileOutputStream(archivo), format);
+            serializer.serialize(doc);
+            // Se ha ejecutado correctamente
+            return 0;
+        } catch (Exception e) {
+      
+            return 1;
+        }   
+    }
+    
+}
